dd89d18470f4178385deea92a23dfa77ad64f76b
pyverts <pyverts@ks363900.kimsufi.com>
2011-11-24 15:30:23 +0100

    first commit

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url('', include('allauth.urls')),
+    url(r'^$', direct_to_template, {'template': 'home.html'}),
+    # Examples:
+    # url(r'^$', 'tutorial.views.home', name='home'),
+    # url(r'^tutorial/', include('tutorial.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+)
