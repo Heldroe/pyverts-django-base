b67214826bf2fbb70272a933fc6e6168bdb8bfa2
Heldroe <heldroe@gmail.com>
2011-11-25 15:41:31 +0100

    now using adapt.js

--- settings.py
+++ settings.py
@@ -68,6 +68,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
